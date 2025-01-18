2de17fe822174c92679c0253d7017665c377096d
Shrek-Loves-Fiona <56651595+Flexecstasy@users.noreply.github.com>
2025-01-18 16:01:16 +0500

    fix settings

--- django_vacancies_statistics/django_vacancies_statistics/settings.py
+++ django_vacancies_statistics/django_vacancies_statistics/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'vacancy_python.urls'
+ROOT_URLCONF = 'django_vacancies_statistics.urls'
 
 TEMPLATES = [
     {
